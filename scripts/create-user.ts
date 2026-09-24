cfb3c328f165db9da9f607dac7d5625be2bddd02
Mohd. Shahul Malik <mohdshahulmalik@gmail.com>
2026-09-24 18:05:25 +0000

    Make create-user script idempotent

--- scripts/create-user.ts
+++ scripts/create-user.ts
@@ -1,8 +1,12 @@
 import { db } from "../lib/db";
 
 async function main() {
-  await db.user.create({ data: { id: "maxum" } });
-  console.log("User 'maxum' created");
+  await db.user.upsert({
+    where: { id: "maxum" },
+    update: {},
+    create: { id: "maxum" },
+  });
+  console.log("User 'maxum' ensured");
   await db.$disconnect();
 }
 
